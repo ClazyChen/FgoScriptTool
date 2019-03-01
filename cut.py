271a728b7d25124c379a969db19679e86f3c232e
ClazyChen <quickb1@163.com>
2019-03-01 19:09:31 +0800

    2.0.1
    修改cut函数，支持1600×900直接截取

--- cut.py
+++ cut.py
@@ -1,26 +1,48 @@
 # -*- coding: utf-8 -*-
 
-from pywinauto.findwindows    import find_window
-from pywinauto.win32functions import ShowWindow, SetForegroundWindow, GetForegroundWindow
-from win32gui import GetWindowRect
-from win32con import SW_RESTORE
-from PIL import ImageGrab
+import win32gui
+import win32ui
+import win32con
+from ctypes import windll
+from PIL import Image
 
 from config import *
 import numpy as np
 
 
-def cut(save_picture=True):
-    hwnd = find_window(title=FGO窗口名)
-    ShowWindow(hwnd, SW_RESTORE)
-    SetForegroundWindow(hwnd)
+# cut函数提供了一个对FGO窗口进行截图的方法
+# 要求FGO窗口处在1600×900的分辨率下
+# 且Mumu模拟器设置为隐藏下方工具栏
+# 这个函数会把上方工具栏截掉，留下1600×900的图像以供后期操作
+def cut(savePicture=True, windowName=FGO窗口名):
+    hwnd = win32gui.FindWindow(None, windowName)
+    left, top, right, bottom = win32gui.GetWindowRect(hwnd)
+    w = right - left
+    h = bottom - top
 
-    # 截取FGO主窗口截图
-    left, top, right, bottom = GetWindowRect(hwnd)
-    src_image = ImageGrab.grab((left, top, right, bottom))
+    # 这个地方需要手动调整
+    # 这里的2代表windows的“显示大小”设置为200%
+    # 因为作者的电脑分辨率过高，所以为200%
+    w *= 2
+    h *= 2
 
-    src_image = src_image.convert('L').convert('RGB')
-    if save_picture:
-        src_image.save('pic.png')
-        print("截图已完成……")
-    return np.array(src_image)
+    hwndDC = win32gui.GetWindowDC(hwnd)
+    mfcDC = win32ui.CreateDCFromHandle(hwndDC)
+    saveDC = mfcDC.CreateCompatibleDC()
+
+    saveBitMap = win32ui.CreateBitmap()
+    saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
+    saveDC.SelectObject(saveBitMap)
+
+    img_dc = mfcDC
+    mem_dc = saveDC
+    mem_dc.BitBlt((0, 0), (w, h), img_dc, (0, 0), win32con.SRCCOPY)
+    bmpInfo = saveBitMap.GetInfo()
+    bmpStr = saveBitMap.GetBitmapBits(True)
+
+    srcImage = Image.frombuffer('RGB', (bmpInfo['bmWidth'], bmpInfo['bmHeight']), bmpStr, 'raw', 'BGRX', 0, 1)
+    srcImage = srcImage.convert('L').convert('RGB').crop((0, h - 900, w, h))
+    if savePicture:
+        srcImage.save('pic.png')
+        print("截图已保存……")
+    return np.array(srcImage)
